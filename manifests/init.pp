8bfe8ce0a363c3cb4b31618a46c9483ff450ec36
n0ts <me@n0ts.org>
2014-01-13 14:25:10 +0900

    Added env script for boxen

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,18 @@
-class brewcask {
-  require homebrew
+require homebrew
+
+class brewcask(
+  $caskroom = "${boxen::config::home}/homebrew-cask/Caskroom"
+) inherits homebrew::config {
+  include boxen::config
 
   homebrew::tap { 'phinze/cask': }
 
   package { 'brew-cask':
     require => Homebrew_Tap['phinze/cask'],
+  }
+
+  boxen::env_script { 'homebrew-cask':
+    content  => template('brewcask/env.sh.erb'),
+    priority => normal,
   }
 }
